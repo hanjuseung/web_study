00419738d3424ee41418d84a2672ab0c70901e82
hanjuseung <rhdqn0501@naver.com>
2025-07-10 13:51:42 +0900

    Request 요청 파라미터 전달(수신) 방법

---
+++ src/main/java/com/app/dto/study/Product.java
@@ -0,0 +1,49 @@
+package com.app.dto.study;
+
+public class Product {
+	
+	public String id;
+	public String name;
+	public int price;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
+	
+	
+	
+	
+	/*
+	getter setter
+	1. private -> getter public
+	2. getsetter 어떤 로직
+	xx.price = -50;
+	xx.setPrice(-50);
+	if( p < 0 )
+	3. spring
+	
+	xx.id = sadd
+	setId(ddd)
+	*/
+}
