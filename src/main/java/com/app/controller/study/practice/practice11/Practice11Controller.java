bddecae9f9450348ca5cbb5dd720170a9ac95ff6
hanjuseung <rhdqn0501@naver.com>
2025-07-16 14:13:27 +0900

    연습11 응용문제 추가 버전

--- src/main/java/com/app/controller/study/practice/practice11/Practice11Controller.java
+++ src/main/java/com/app/controller/study/practice/practice11/Practice11Controller.java
@@ -2,21 +2,31 @@ package com.app.controller.study.practice.practice11;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.websocket.server.PathParam;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 
 @Controller
 public class Practice11Controller {
 	
 	@GetMapping("/practice11/first")
-	public String prac11(HttpServletRequest request) {
+	public String prac11(Model model,HttpServletRequest request) {
 		
 		HttpSession session = request.getSession();
-		session.setAttribute("accessUrl", "/first");
+		
+		if(session.getAttribute("accessUrl")==null) {
+			model.addAttribute("accessUrl", "/first");
+		} else {
+			model.addAttribute("accessUrl",session.getAttribute("accessUrl"));
+			
+//			session.removeAttribute("accessUrl"); //특정 키값으로 삭제
+			session.invalidate(); //세션 클리어
+			
+		}
 
+		
 		return "practice/practice11/targetPage";
 	}
 	@GetMapping("/practice11/firsthide1")
@@ -29,6 +39,15 @@ public class Practice11Controller {
 	}
 	@GetMapping("/practice11/firsthide2")
 	public String prac11h2(HttpServletRequest request) {
+		return "redirect:/practice11/first";
+	}
+	
+	// practice12 응용버전 추가
+	@GetMapping("/practice11/firsthide3")
+	public String prac11h3(HttpSession session) {
+
+		session.setAttribute("accessUrl", "/firsthide3");
+		
 		return "redirect:/practice11/first";
 	}
 }
