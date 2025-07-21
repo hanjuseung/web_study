d63012c1e091917d8dd435aa67c06ccf92683a7b
hanjuseung <rhdqn0501@naver.com>
2025-07-21 12:22:48 +0900

    객실 신규 등록, 유저 서비스 구조 추가

---
+++ src/main/java/com/app/controller/admin/adminController.java
@@ -0,0 +1,30 @@
+package com.app.controller.admin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.app.dto.room.Room;
+import com.app.service.room.RoomService;
+
+@Controller
+public class adminController {
+	@Autowired
+	RoomService roomService;
+
+	@GetMapping("/admin/registerRoom")
+	public String registerRoom() {
+		return "admin/registerRoom";
+	}
+	
+	@PostMapping("/admin/registerRoom")
+	public String registerRoom(Room room) {
+		//값이 잘 넘어왔는가
+		System.out.println(room.toString());
+		
+		//room 정보 등록
+		int result = roomService.saveRoom(room);
+		return "admin/registerRoom";
+	}
+}
